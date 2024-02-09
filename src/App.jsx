1c00f13d17569c4282a4707768cba7bc742a8485
Ryan Kienstra <kienstraryan@gmail.com>
2024-02-09 17:29:28 -0600

    Create the first example of useEffect to sync state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useEffect } from 'react'
-import { useAppContext } from './Store/context'
-import { NavigationComponent } from './Navigation/NavigationComponent'
-import { HomeComponent } from './Navigation/Home/HomeComponent'
-import { LoginRegisterComponent } from './Authentication/LoginRegisterComponent'
+import { useEffect, useState } from 'react'
 import React from 'react'
 
+function toSlug(x) {
+  return x
+    ?.toLowerCase()
+    .replace( /[^\w ]+/g, '' )
+    .replace( / +/g, '-' )
+    .replace( /_+/g, '-' )
+}
+
 export default function App() {
+  const [name, setName] = useState('')
+  const [slug, setSlug] = useState('')
+
+  useEffect(() => {
+    setSlug(toSlug(name))
+  }, [name])
+
   return (
-    <div className="container">
+    <div>
+      <label htmlFor="field-name">
+        Name
+      </label>
+      <input
+        type="text"
+        id="field-name"
+        value={name}
+        onChange={(event) => {
+          event.preventDefault()
+          setName(event.target.value)
+        }}
+      />
+      <p>Slug {slug}</p>
     </div>
   )
 }
